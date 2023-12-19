a527fac176955eb547b9a37c27cd25fafbe6b033
karlexceed <alex.decker@gmail.com>
2023-12-19 06:14:58 -0600

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,18 +7,33 @@ local S = minetest.get_translator(minetest.get_current_modname())
 local old_handle_node_drops = minetest.handle_node_drops
 
 minetest.handle_node_drops = function(pos, drops, digger)
-	local inv = digger:get_inventory()
-	
-	-- Check player inventory for rune
-	if inv:contains_item("main", "miner_runes:repel_stone") then
-		local playername = digger:get_player_name()
-		
-		-- Check oldnode's group for stone
-		for k, name in pairs(drops) do
-			if minetest.get_item_group(name, "stone") > 0 then
+	-- Check drops
+	for k, name in pairs(drops) do
+		-- Is it stone?
+		if minetest.get_item_group(name, "stone") > 0 then
+			-- Is the player holding the Repel Stone rune?
+			local inv = digger:get_inventory()
+			
+			if inv:contains_item("main", "miner_runes:repel_stone") then
+				local playername = digger:get_player_name()
+				
 				-- Don't pick up the node!
 				drops[k] = nil
-				local handle = minetest.sound_play({name = "miner_runes_vaporize"},{to_player = playername})
+				local handle = minetest.sound_play({name = "miner_runes_vaporize2"},{to_player = playername})
+			end
+		end
+		
+		-- Is it crumbly?
+		if minetest.get_item_group(name, "crumbly") > 0 then
+			-- Is the player holding the Repel Crumbly rune?
+			local inv = digger:get_inventory()
+			
+			if inv:contains_item("main", "miner_runes:repel_crumbly") then
+				local playername = digger:get_player_name()
+				
+				-- Don't pick up the node!
+				drops[k] = nil
+				local handle = minetest.sound_play({name = "miner_runes_vaporize2"},{to_player = playername})
 			end
 		end
 	end
@@ -26,7 +41,9 @@ minetest.handle_node_drops = function(pos, drops, digger)
 	return old_handle_node_drops(pos, drops, digger)
 end
 
--- Define the object
+
+-- Define the objects
+-- Repel Stone rune
 minetest.register_craftitem("miner_runes:repel_stone", {
     description = "Stone repelling rune",
     inventory_image = "miner_runes_jeran_16.png"
@@ -39,5 +56,21 @@ minetest.register_craft({
         {"group:stone", "", "group:stone"},
         {"", "group:soil", ""},
         {"group:stone", "", "group:stone"}
+    }
+})
+
+-- Repel Dirt, Sand, and Gravel Rune
+minetest.register_craftitem("miner_runes:repel_crumbly", {
+    description = "Dirt, sand, and gravel repelling rune",
+    inventory_image = "miner_runes_haglaz_16.png"
+})
+
+minetest.register_craft({
+    type = "shaped",
+    output = "miner_runes:repel_crumbly",
+    recipe = {
+        {"group:crumbly", "", "group:crumbly"},
+        {"", "group:stone", ""},
+        {"group:crumbly", "", "group:crumbly"}
     }
 })
